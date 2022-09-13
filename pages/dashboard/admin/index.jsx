2c2b4175eb0d17df62365c79998e5780281cf78f
Sonephet Minalavong <sonephet.sm@laogw.la>
2022-09-13 09:20:08 +0700

    FEAT: add layout

--- pages/dashboard/admin/index.jsx
+++ pages/dashboard/admin/index.jsx
@@ -1,13 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import AdminLayout from "../../../components/layouts/adminLayout";
 
 const Admin = () => {
   return (
-    <>
+    <AdminLayout>
       <Link href="/dashboard/admin/products">Product</Link>
       <Link href="/dashboard/admin/products/add">New product</Link>
       <div>Admin</div>
-    </>
+    </AdminLayout>
   );
 };
 
---
+++ pages/dashboard/user/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import UserLayout from "../../../components/layouts/userLayout";
+
+const User = () => {
+  return <UserLayout>User</UserLayout>;
+};
+
+export default User;
