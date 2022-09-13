9c12a428141059405566756d1507fd748547d959
Sonephet Minalavong <sonephet.sm@laogw.la>
2022-09-13 09:20:49 +0700

    FEAT: add layout

--- pages/dashboard/admin/products/add.jsx
+++ pages/dashboard/admin/products/add.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import AdminLayout from "../../../../components/layouts/adminLayout";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  return <AdminLayout>AddProduct</AdminLayout>;
 };
 
 export default AddProduct;
